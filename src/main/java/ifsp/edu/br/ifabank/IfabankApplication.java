9b77434ba104c0b02949aa2f6f2a1ccad05f4645
henriquescuzate <henriquescuzate5s@gmail.com>
2026-06-05 20:53:10 -0300

    criando projeto ifbank spring boot

---
+++ src/main/java/ifsp/edu/br/ifabank/IfabankApplication.java
@@ -0,0 +1,13 @@
+package ifsp.edu.br.ifabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfabankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfabankApplication.class, args);
+    }
+
+}
