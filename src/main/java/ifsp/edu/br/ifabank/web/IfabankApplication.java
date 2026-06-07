82155c7e075ffd901e5e0b3fc69b7eefd45914d9
henriquescuzate <henriquescuzate5s@gmail.com>
2026-06-07 12:08:17 -0300

    Reorganiza estrutura inicial do backend

--- src/main/java/ifsp/edu/br/ifabank/web/IfabankApplication.java
+++ src/main/java/ifsp/edu/br/ifabank/web/IfabankApplication.java
@@ -1,4 +1,4 @@
-package ifsp.edu.br.ifabank;
+package ifsp.edu.br.ifabank.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
