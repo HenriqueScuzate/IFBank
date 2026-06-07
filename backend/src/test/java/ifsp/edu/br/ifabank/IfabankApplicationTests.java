5feda163a51af44add43a739eeaae338caec7a9e
henriquescuzate <henriquescuzate5s@gmail.com>
2026-06-07 13:03:34 -0300

    Reorganiza projeto com backend e frontend

---
+++ backend/src/test/java/ifsp/edu/br/ifabank/IfabankApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsp.edu.br.ifabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfabankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
